03929faf52f6a04336c2573a9c18cc31bf69a5b7
Daniel <db_win10_login@outlook.com>
2019-02-21 17:09:41 +0000

    works

--- eveSkill.py
+++ eveSkill.py
@@ -3,19 +3,8 @@
 from html import parser
 from enum import Enum
 import sqlite3 as sql
+import sys
 
-
-f=open("Spaceship Command.eveuni.html")
-DEBUG=False
-class SkillType(Enum):
-    none = 1
-    name = 2
-    desc = 3
-    multiplier = 4
-    primaryID = 5
-    secondaryID = 6
-    groupID = 7
-    
 class SkillParser(parser.HTMLParser):
     def clearall(self):
         self.tagStack=[]
@@ -23,18 +12,35 @@ class SkillParser(parser.HTMLParser):
         self.stackMatching=[
             (["img","span","h2","div","div","div","body","html"],None,False,"name"),
             (["div","div","div","div","body","html"],None,False,"desc"),
-            (["span","td","tr","table","div","div","div","div","body","html"],None,"title","primaryAttribute"),
-            (["span","td","tr","table","div","div","div","div","body","html"],None,"title","secondaryAttribute"),
+            (["span","td","tr","table","div","div","div","div","body","html"],None,"title","primaryID"),
+            (["span","td","tr","table","div","div","div","div","body","html"],None,"title","secondaryID"),
             (["span","td","tr","table","div","div","div","div","body","html"],{"style": "cursor: help;"},False,"multiplier"),
             (["span","td","tr","table","div","div","div","div","body","html"],{"style": "cursor: help;"},False,"cost")
         ]
-        self.results=[]
+        self.results=[dict()]
         self.lookingFor=0  
         self.printNext=0
+        self.mdata=""
+        self.conn=sql.connect("eveSkill.s3db")
+        self.c=self.conn.cursor()
         
+    def setGroup(self,group):
+        r=self.c.execute("SELECT id FROM skillGroup WHERE name LIKE ? ;",(group,))
+        f=r.fetchall()
+        if not f:
+            print("Group not found: %s" % (group) )
+            return
+        self.groupID=f[0][0]
+        self.groupName=group
+        
+        print("Group ID %i set." % (self.groupID) )
     def handle_starttag(self,tag,attrs):
         self.tagStack.insert(0,tag)
         self.attrStack.insert(0,attrs)
+        if self.lookingFor == len(self.stackMatching):
+            self.lookingFor=0
+            self.processResults()
+            self.results.append(dict())
         m=self.stackMatching[self.lookingFor]
         match=False
         if len(self.tagStack) == len(m[0]):
@@ -45,8 +51,6 @@ class SkillParser(parser.HTMLParser):
                     break
             if m[1]:
                 for j in m[1].keys():
-##                    print("testing for %s in:" % (str(j)))
-##                    print(attrs)
                     a=None
                     for i in attrs:
                         if i[0] == j:
@@ -59,18 +63,16 @@ class SkillParser(parser.HTMLParser):
                         break
                     
         if match:
-##            print("Match: %s" % (m[-1]))
-            self.lookingFor+=1
-            if self.lookingFor == len(self.stackMatching):
-                self.lookingFor=0
-            self.results.append(dict())
+
             
             if m[2]:
                 for i in attrs:
                     if m[2]==i[0]:
-                        print(i[1])
-                
-            self.printNext=2
+                        self.results[-1][m[-1]]=i[1]
+                        self.lookingFor+=1
+                        break
+            else:
+                self.printNext=2
         
                 
     def handle_endtag(self,tag):
@@ -79,13 +81,52 @@ class SkillParser(parser.HTMLParser):
         self.attrStack.pop(i)
         if(self.printNext>0):
             self.printNext-=1
+            if(self.printNext == 0):
+                self.results[-1][self.stackMatching[self.lookingFor][-1]]=self.mdata
+                self.mdata=""
+                self.lookingFor+=1
+                
         
     def handle_data(self,data):
         if self.printNext>0:
             d=data.strip('\n')
             if d:
-                print(d)
+                self.mdata+=d
+
+
+    def processResults(self):
+        name=self.results[-1]['name']
+        desc=self.results[-1]['desc']
         
-parser = SkillParser()
-parser.clearall()
+        primaryAttribute=self.results[-1]['primaryID'].split(' ')[2]
+        self.c.execute("SELECT id FROM attribute WHERE name LIKE ? ;",(primaryAttribute,))
+        primaryID=self.c.fetchall()[0][0]
+        
+        secondaryAttribute=self.results[-1]['secondaryID'].split(' ')[2]
+        self.c.execute("SELECT id FROM attribute WHERE name LIKE ? ;",(secondaryAttribute,))
+        secondaryID=self.c.fetchall()[0][0]
+        
+        multiplier=self.results[-1]['multiplier'].rstrip('x')
+        cost=self.results[-1]['cost'].rstrip('\xa0ISK')
+        self.c.execute(
+'''
+INSERT INTO skill( name, desc, primaryID, secondaryID, groupID, multiplier, cost )
+VALUES( ?, ?, ?, ?, ?, ?, ?);
+''', ( name, desc, primaryID, secondaryID, self.groupID, multiplier, cost ) )
+##        print( "---RESULT:---\nName: %s\n Desc: %s\n Primary: %s [%s]\nSecondary %s [%s]\nMultiplier: %s Cost: %s" %
+##               ( name, desc, primaryAttribute, primaryID, secondaryAttribute, secondaryID, multiplier, cost ) )
+        self.conn.commit()
+                               
+        
+for file in sys.argv[1:]:
+    try:
+        print("File: %s" % (file))
+        p = SkillParser()
+        p.clearall()
+        p.setGroup(file.replace('_',' '))
+        f = open(file)
+        p.feed(f.read())
+        f.close()
+    except Exception as e:
+        print(e)
 
